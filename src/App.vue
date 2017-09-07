c8ed956ff4fc9ac310fa0e40cdd985d3f69597d7
Liam Tarpey <liam.m.tarpey@gmail.com>
2017-09-07 23:04:14 +0100

    New app structure

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+    <div id="app">
+        <Test></Test>
+    </div>
+</template>
+
+<script>
+    import Test from './components/Test.vue';
+
+    const App = {
+        components: {
+            Test: Test
+        }
+    };
+
+    export default App;
+</script>
---
+++ src/components/Test.vue
@@ -0,0 +1,17 @@
+<template>
+    <h1>Bonjour.</h1>
+</template>
+
+<script>
+
+    const Test = {
+        data: function() {
+            return {};
+        }
+    };
+
+    export default Test;
+</script>
+
+<style>
+</style>
